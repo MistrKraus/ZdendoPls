8dd4343292e87c8bc4d5c4e6f114d20eb7af80d1
petr <stechy12@gmail.com>
2017-01-15 16:25:16 +0100

    Finální verze. Pro javaFX build použij příkaz: gradlew.bat jfxjar To vygeneruje spustitelný jar soubor. Dále akorát musíš k tomu souboru přikopírovat ručne ty zvuky a obrázky (složky)

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,63 +1,64 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.ImagePattern;
 import javafx.stage.Stage;
 
-import java.applet.Applet;
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Main extends Application {
 
-    private static URL[] sounds;
-    private static Random random = new Random();
-    private static int id = 3;
+    private List<URL> sounds = new ArrayList<>();
+    private Random random = new Random();
+    private int id = 3;
 
-    private static void loadSounds() {
-        File folder = new File("sounds");
-        File[] listOfFiles = folder.listFiles();
+    private void loadSounds() {
+        File tmp = new File(new File("").getAbsolutePath() + "/sounds");
+        System.out.println(tmp.getAbsolutePath());
+        File[] listOfFiles = new File(tmp.getPath()).listFiles();
         assert(listOfFiles != null);
-        URL[] temp = new URL[listOfFiles.length];
 
-        int j = 0;
-        for (File file : listOfFiles) {
-            if (file.isFile() && file.getName().endsWith(".wav"))
-                try {
-                    temp[j++] = new URL("file:" + file.getPath());
-                } catch (Exception e) {
-                    System.out.println("Chyba pri cteni souboru!");
-                }
-        }
-
-        sounds = new URL[j];
-
-        System.arraycopy(temp, 0, sounds, 0, j);
+        Arrays.stream(listOfFiles)
+                .filter(file -> file.getName().endsWith(".wav"))
+                .forEach(file -> {
+                    try {
+                        sounds.add(new URL("file:" + file.getPath()));
+                    } catch (Exception e) {
+                        System.out.println("Chyba pri cteni souboru!");
+                    }
+                });
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        loadSounds();
         String image = new File("images/zdendoPls4.png").getPath();
-
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
+        Parent root = new StackPane();
         ImagePattern img = new ImagePattern(new Image(image));
         Scene scene = new Scene(root, 300, 275);
         scene.setFill(img);
         scene.setOnMouseClicked(event -> {
             try {
-                int temp;
+                int temp = Main.this.sounds.size();
                 do {
-                    temp = random.nextInt(sounds.length);
+                    temp = random.nextInt(temp);
                 } while (temp == id);
                 id = temp;
 
-                Applet.newAudioClip(sounds[id]).play();
+                Media media = new Media(Main.this.sounds.get(id).getPath());
+                MediaPlayer mediaPlayer = new MediaPlayer(media);
+                mediaPlayer.play();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
